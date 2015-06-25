16ad57a63d647651ce9c88a46e40d3a322209c9b
jmsmrgn <jmorg47@gmail.com>
2015-06-25 00:00:06 -0700

    add controllers to mad libs app

--- mad-libs/refactor-1/app/js/app.js
+++ mad-libs/refactor-1/app/js/app.js
@@ -1,1 +1,28 @@
-var app = angular.module('myApp', []);
+angular.module('myApp', [])
+  .controller('myCtrl', function($scope) {
+    $rootScope = [{
+      name: 'name',
+      job_title: 'job title',
+      tedious_task: 'tedious task',
+      dirty_task: 'dirty task',
+      celebrity: 'celebrity',
+      useless_skill: 'useless skill',
+      obnoxious_celebrity: 'obnoxious celebrity',
+      huge_number: 'huge number',
+      adjective: 'adjective'
+    }];
+  })
+  .controller('whichVersion', function($scope) {
+    $scope.template = function(gender) {
+      console.log(gender);
+      if (gender === 'male') {
+        $('.template').addClass('template-enable');
+        $('.male-version').show();
+        $('.female-version').hide();
+      } else if (gender === 'female') {
+        $('.template').addClass('template-enable');
+        $('.female-version').show();
+        $('.male-version').hide();
+      }
+    };
+  });
