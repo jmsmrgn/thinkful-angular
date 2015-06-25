028b1ac259d2cee3ec5907895ba6c58fd5c00835
jmsmrgn <jmorg47@gmail.com>
2015-06-25 11:17:01 -0700

    mad libs refactor 2 - wip

--- mad-libs/refactor-2/app/js/app.js
+++ mad-libs/refactor-2/app/js/app.js
@@ -1,5 +1,6 @@
 angular.module('myApp', [])
-  .controller('myCtrl', function($scope) {
+
+  .controller('ctrl1', function($scope) {
     $rootScope = [{
       name: 'name',
       job_title: 'job title',
@@ -12,17 +13,32 @@ angular.module('myApp', [])
       adjective: 'adjective'
     }];
   })
-  .controller('whichVersion', function($scope) {
+
+  .controller('ctrl2', function($scope) {
     $scope.template = function(gender) {
       console.log(gender);
       if (gender === 'male') {
-        $('.template').addClass('template-enable');
+        $('.user-inputs').addClass('disabled');
+        $('.template').removeClass('disabled');
         $('.male-version').show();
         $('.female-version').hide();
       } else if (gender === 'female') {
-        $('.template').addClass('template-enable');
+        $('.user-inputs').addClass('disabled');
+        $('.template').removeClass('disabled');
         $('.female-version').show();
         $('.male-version').hide();
       }
-    };
+    }
+
+    // $scope.hideForm = false;
+
+    // $scope.submit = function(){
+    //   $scope.hideForm = true;
+    // }
+
+    // $scope.reset = function(){
+    //   // $scope.data = {};
+    //   $scope.hideForm = false;
+    //   $scope.submitted = false;
+    // }
   });
