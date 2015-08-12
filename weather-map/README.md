ea1a886215ba51685f4b5e6fca1d6147ee42d59b
jmsmrgn <jmorg47@gmail.com>
2015-08-12 15:19:09 -0700

    Initialize open weather map app

---
+++ weather-map/README.md
@@ -0,0 +1,5 @@
+## 2.1.1
+
+Open Weather Map App
+
+Run `npm install` then `bower install` then `gulp`
---
+++ weather-map/app/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module('weatherMap', [])
+
+  app.controller('WeatherMapCtrl', function($scope) {
+    $scope.greeting = "Hello World!";
+  });
