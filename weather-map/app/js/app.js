6571194f0bd1d2e37388c22e41dfdb6904ea9b74
jmsmrgn <jmorg47@gmail.com>
2015-08-12 16:24:44 -0700

    Weather map app checkpoint 1

--- weather-map/app/js/app.js
+++ weather-map/app/js/app.js
@@ -1,5 +1,31 @@
-var app = angular.module('weatherMap', [])
-
-  app.controller('WeatherMapCtrl', function($scope) {
-    $scope.greeting = "Hello World!";
+var app = angular.module('weatherMap', ['ngRoute'])
+  .value('appCities', ['New York', 'Dallas', 'Chicago'])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+      templateUrl : 'home.html',
+      controller : 'HomeCtrl'
+    })
+    .when('/cities/:city', {
+      templateUrl : 'city.html',
+      controller : 'CityCtrl',
+      resolve : {
+        city: function(appCities, $route, $location) {
+          var city = $route.current.params.city;
+          if(appCities.indexOf(city) == -1 ) {
+            $location.path('/error');
+            return;
+          }
+          return city;
+        }
+      }
+    })
+    .when('/error', {
+      template : '<p>Error - Page Not Found</p>'
+    });
+  }])
+  .controller('HomeCtrl', function($scope) {
+    //empty for now
+  })
+  .controller('CityCtrl', function($scope, city) {
+    $scope.city = city;
   });
